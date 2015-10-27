d3cd2a48a09ca5a8f54a30e379bddc8a29219d2d
Koding <jason.norman@gmail.com>
2015-10-27 21:00:13 +0000

    Moving x, y, rad, color and borderColor properties into the Shape object. Preserving starting X and Y values in shape so the pressup function can return the shape to it's original location. Fixing tests.

--- app/scripts/model/NoteMarker.js
+++ app/scripts/model/NoteMarker.js
@@ -2,11 +2,13 @@ var ns = HealingHz.createNS("HealingHz.model");
 
 ns.NoteMarker = function(x, y, rad, color, borderColor) {
     this.circle = new createjs.Shape();
+    this.circle.origx = x;
+    this.circle.origy = y;
     this.circle.x = x;
     this.circle.y = y;
-    this.rad = rad;
-    this.color = color;
-    this.borderColor = borderColor;
+    this.circle.rad = rad;
+    this.circle.color = color;
+    this.circle.borderColor = borderColor;
 };
 
 ns.NoteMarker.prototype.draw = function(stage) {
@@ -14,9 +16,9 @@ ns.NoteMarker.prototype.draw = function(stage) {
     var c = this.circle;
 
     c.graphics
-        .beginStroke(this.borderColor)
-        .beginFill(this.color)
-        .drawCircle(0, 0, this.rad);
+        .beginStroke(c.borderColor)
+        .beginFill(c.color)
+        .drawCircle(0, 0, c.rad);
 
     c.on("pressmove", 
         function(evt) {
@@ -26,7 +28,8 @@ ns.NoteMarker.prototype.draw = function(stage) {
         });
 
     c.on("pressup", function(evt) { 
-
+        var hits = 0;
+        
         for(i=0; i< HealingHz.markerBoxes.length; i++)
         {
             var markerBox = HealingHz.markerBoxes[i];
@@ -34,16 +37,24 @@ ns.NoteMarker.prototype.draw = function(stage) {
 
             if(markerBox.box.hitTest(pt.x, pt.y)) {
                
+               hits++;
+               
                if(!markerBox.hasNoteMarker()) {
                     evt.target.x = markerBox.box.x + (markerBox.w/2);
                     evt.target.y = markerBox.box.y + (markerBox.h/2);
                }
                else {
-                   evt.target.x = evt.target.x+35;
-                   evt.target.y = evt.target.y+35;
+                   evt.target.x = evt.target.origx;
+                   evt.target.y = evt.target.origy;
                }
             }
         }
+        
+        if(hits === 0) {
+            evt.target.x = evt.target.origx;
+            evt.target.y = evt.target.origy;
+        }
+        
         stage.update();
         
         // Find the clicked NoteMarker
@@ -55,7 +66,7 @@ ns.NoteMarker.prototype.draw = function(stage) {
                 console.log(note.circle.x);
             }
         }
-    })
+    });
 
     stage.addChild(c);
     stage.update();
